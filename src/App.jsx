17fee3be9af6b6786739e2772137e642db171955
denis-home <denisss.dev@gmail.com>
2026-02-26 20:05:29 -0300

    resolucao bug memoria android-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import Album from "./pages/Album";
-import PhotoBooth from "./pages/PhotoBooth";
 import JogoMemoria from "./pages/JogoMemoria";
 import SuperHeitor from "./pages/SuperHeitor";
 import Contatos from "./pages/Contatos";
 import "./App.css";
+
+const PhotoBooth = React.lazy(() => import("./pages/PhotoBooth"));
 
 export default function App() {
   return (
@@ -15,14 +17,16 @@ export default function App() {
       <div className="appShell">
         <Header />
         <main className="appMain">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/super-heitor" element={<SuperHeitor />} />
-            <Route path="/album" element={<Album />} />
-            <Route path="/album-de-fotos" element={<PhotoBooth />} />
-            <Route path="/jogo" element={<JogoMemoria />} />
-            <Route path="/contatos" element={<Contatos />} />
-          </Routes>
+          <Suspense fallback={null}>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/super-heitor" element={<SuperHeitor />} />
+              <Route path="/album" element={<Album />} />
+              <Route path="/album-de-fotos" element={<PhotoBooth />} />
+              <Route path="/jogo" element={<JogoMemoria />} />
+              <Route path="/contatos" element={<Contatos />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer />
       </div>
